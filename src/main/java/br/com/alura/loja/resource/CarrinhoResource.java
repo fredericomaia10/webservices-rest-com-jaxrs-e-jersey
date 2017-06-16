7371e4909490a54dde1541da3ed92ca6e7a5fe87
Frederico Maia Arantes <frederico.maia@tecsinapse.com.br>
2017-06-16 19:48:59 -0300

    Aula 3

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -3,17 +3,16 @@ package br.com.alura.loja.resource;
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("carrinhos")
 public class CarrinhoResource {
     @GET
+    @Path("{id}")
     @Produces(MediaType.APPLICATION_XML)
-    public String busca() {
-        Carrinho carrinho = new CarrinhoDAO().busca(1l);
+    public String busca(@PathParam("id") long id) {
+        Carrinho carrinho = new CarrinhoDAO().busca(id);
         return carrinho.toXML();
     }
 }
