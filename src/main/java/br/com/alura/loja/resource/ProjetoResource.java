01f7f1fd75b8cebc09c5677770687ffffcbdc1e5
Frederico Maia Arantes <frederico.maia@tecsinapse.com.br>
2017-06-17 13:49:32 -0300

    Aula 9

--- src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -27,4 +27,11 @@ public class ProjetoResource {
         URI uri = URI.create("/projetos/" + projeto.getId());
         return Response.created(uri).build();
     }
+
+    @Path("{id}")
+    @DELETE
+    public Response removeProduto(@PathParam("id") long id) {
+        new ProjetoDAO().remove(id);
+        return Response.ok().build();
+    }
 }
